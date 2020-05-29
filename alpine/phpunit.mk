7499717a69fb700b7ca42949e652c8e4ce7d1609
George Sokianos <walkero@gmail.com>
2020-05-29 15:01:28 +0100

    Added generic makefile and dockerfile

---
+++ alpine/phpunit.mk
@@ -0,0 +1,18 @@
+PHPUNIT_VER := 8.5.5
+ifneq ($(origin UNIT),undefined)
+	ifeq ($(UNIT),9)
+		PHPUNIT_VER := 9.1.5
+	endif
+	ifeq ($(UNIT),8)
+		PHPUNIT_VER := 8.5.5
+	endif
+	ifeq ($(UNIT),7)
+		PHPUNIT_VER := 7.5.19
+	endif
+	ifeq ($(UNIT),6)
+		PHPUNIT_VER := 6.5.14
+	endif
+	ifeq ($(UNIT),5)
+		PHPUNIT_VER := 5.7.27
+	endif
+endif
