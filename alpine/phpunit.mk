eb0d1f04ddbfc243946c91058f03bd5591ce9836
George Sokianos <George.Sokianos@hostelworld.com>
2022-04-14 16:51:58 +0100

    Added newest phpunit versions and fixed an issue the images had with the older builds

--- alpine/phpunit.mk
+++ alpine/phpunit.mk
@@ -1,13 +1,13 @@
 PHPUNIT_VER := 8.5.5
 ifneq ($(origin UNIT),undefined)
 	ifeq ($(UNIT),9)
-		PHPUNIT_VER := 9.1.5
+		PHPUNIT_VER := 9.5.20
 	endif
 	ifeq ($(UNIT),8)
-		PHPUNIT_VER := 8.5.5
+		PHPUNIT_VER := 8.5.26
 	endif
 	ifeq ($(UNIT),7)
-		PHPUNIT_VER := 7.5.19
+		PHPUNIT_VER := 7.5.20
 	endif
 	ifeq ($(UNIT),6)
 		PHPUNIT_VER := 6.5.14
